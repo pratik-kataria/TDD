2f89bb02cb8a9013a3482310b900dda320a75e70
Pratik Kataria <pratik.bhanushali355@gmail.com>
2020-05-22 00:32:31 +0530

    Empty String test case

---
+++ src/main/java/tdd/StringCalculator.java
@@ -0,0 +1,18 @@
+package tdd;
+
+public class StringCalculator {
+	
+	public StringCalculator()
+	{
+		
+	}
+
+	public int add(String s) {
+		if(s.equals(""))
+		{
+			return 0;
+		}
+		return 0;
+	}
+
+}
--- src/test/java/tdd/StringCalculatorTest.java
+++ src/test/java/tdd/StringCalculatorTest.java
@@ -1,5 +1,24 @@
 package tdd;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringCalculatorTest {
+
+	private StringCalculator stringCalculator;
+	
+	@Before
+	public void setup()
+	{
+		stringCalculator = new StringCalculator();
+	}
+	@Test
+	public void emptyStringTest()
+	{
+		String s = "";
+		int returnedValue = stringCalculator.add(s);
+		Assert.assertEquals(returnedValue, 0);
+	}
 
 }
